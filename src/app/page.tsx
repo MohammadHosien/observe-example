41a46155fa1743adcb3721c0fd83daca25e68807
mohoArtaneh <mohoartan81@gmail.com>
2023-11-19 15:37:18 +0330

    make some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,77 +4,64 @@ import { useEffect, useState, useRef } from "react";
 
 export default function Home() {
   const [data, setData] = useState<any>([]);
-  const [page, setPage] = useState(0);
   const [hasMore, sethasMore] = useState(true);
   const [loading, setLoading] = useState(true);
-
-  const element = useRef(null);
-
-  const exmaple = async () => {
-    setLoading(false);
-    const req = await fetch(
-      `https://dummyjson.com/products?limit=10&skip=${page * 10}`
-    );
-    const res = await req.json();
-    console.log(res);
-    if (res.products.length == 0) {
-      sethasMore(false);
-    } else {
-      setData((prev: any) => [...prev, ...res.products]);
-      setPage(prev=>prev + 1);
-    }
-    
-    
-  };
+  const element = useRef<HTMLDivElement>(null);
+  
 
   useEffect(() => {
-    
-    const observe = new IntersectionObserver((entires) => {
-      const firstEnt = entires[0];
-      if (firstEnt.isIntersecting && hasMore) {
-        exmaple();
-      }
-    });
-    if (observe && element) {
-      observe.observe(element.current);
+    let page = -1;
+    console.log(element.current?.innerText === "");
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          (async () => {
+            page++;
+            setLoading(false);
+            if (element.current?.innerText !== "") {
+              console.log("hello")
+              
+              const req = await fetch(
+                `https://dummyjson.com/products?limit=10&skip=${page * 10}`
+              );
+              const res = await req.json();
+              if (res.products.length == 0) {
+                return sethasMore(false);
+              }
+              setData((prevItems: any) => [...prevItems, ...res.products]);
+              setLoading(true);
+            }
+          })();
+        }
+      },
+      { threshold: 1 }
+    );
+
+    if (element.current) {
+      observer.observe(element.current);
     }
     return () => {
-      if (observe) {
-        observe.disconnect();
+      if (element.current) {
+        observer.unobserve(element.current);
+        observer.disconnect();
       }
     };
-  },[data]);
-
-  console.log(hasMore)
-
-  // useEffect(() => {
-  //   (async () => {
-  //     setLoading(false);
-  //     const req = await fetch(
-  //       `https://dummyjson.com/products?limit=10&skip=${page * 10}`
-  //     );
-  //     const res = await req.json();
-  //     console.log(res);
-  //     if (res.products.length == 0) {
-  //       sethasMore(false);
-  //     } else {
-  //       setData((prev: any) => [...res.products]);
-  //     }
-  //   })();
-  // }, []);
+  }, [element.current]);
 
   return (
     <>
       {data.map((item: any) => (
-        <>
-          <img src={item.images[0]} />
+        <div className="text-center max-h-screen">
+          <img className="mx-auto" src={item.images[0]} />
           <div>{item.title}</div>
-        </>
+        </div>
       ))}
-      {hasMore && (
-        <div className="" ref={element}>
+      {hasMore && loading ? (
+        <div className=" text-center text-[40px] bg-slate-800" ref={element}>
           loading
         </div>
+      ) : (
+        <div ref={element} className=" text-center text-[40px] w-32 h-14"></div>
       )}
     </>
   );
